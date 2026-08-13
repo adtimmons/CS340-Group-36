d2d5d807aab29fc53ab368d719b445cf9a6c8b04
Alysha Timmons <timmonal@classwork.engr.oregonstate.edu>
2026-08-12 17:19:05 -0700

    Update

--- CS340Group36DataManipulationQueries.sql
+++ CS340Group36DataManipulationQueries.sql
@@ -10,249 +10,332 @@ SELECT lineID, lineName, department
 FROM AssemblyLines;
 
 /* Add new Line */
-
-INSERT INTO AssemblyLines (lineName, department)
-Values (@lineName_from_form, @department_from_form);
+CALL sp_create_assembly_line(
+    @lineName_from_form,
+    @department_from_form,
+    @lineID_out
+);
 
 /* Delete a Line */
+CALL sp_delete_assembly_line(
+    @lineID_of_row_of_button
+);
 
-DELETE FROM AssemblyLines
-WHERE lineID = @lineID_of_row_of_button;
-
-/* Edit a line pre-fill*/
-
+/* Edit a line pre-fill */
 SELECT lineID, lineName, department
 FROM AssemblyLines
 WHERE lineID = @lineID_of_row_of_button;
 
 /* Update the line */
+CALL sp_update_assembly_line(
+    @lineID_of_row_of_button,
+    @lineName_from_form,
+    @department_from_form
+);
 
-UPDATE AssemblyLines
-SET lineID = @lineName_from_form, department = @department_from_form
-WHERE lineID = @lineID_of_row_of_button;
 
 /* ---------------------------------------------------
 Team Members SQL
 -----------------------------------------------------*/
 
-/* Read lines */
-
-SELECT TeamMembers.teamMemberID, TeamMembers.firstName, TeamMembers.lastName, AssemblyLines.lineName, TeamMembers.employmentStatus
+/* Read Team Members */
+SELECT TeamMembers.teamMemberID,
+       TeamMembers.firstName,
+       TeamMembers.lastName,
+       TeamMembers.homeLineID,
+       AssemblyLines.lineName,
+       TeamMembers.employmentStatus
 FROM TeamMembers
-INNER JOIN AssemblyLines ON TeamMembers.homeLineID = AssemblyLines.lineID;
+INNER JOIN AssemblyLines
+    ON TeamMembers.homeLineID = AssemblyLines.lineID;
 
 /* Read for Home line Dropdown */
-
-SELECT lineID, lineName FROM AssemblyLines;
+SELECT lineID, lineName
+FROM AssemblyLines;
 
 /* Add new Team Member */
-
-INSERT INTO TeamMembers (firstName, lastName, homeLineID, employmentStatus)
-VALUES (@firstName_from_form, @lastName_from_form, @homeLineID_from_dropdown, @employmentStatus_from_dropdown);
+CALL sp_create_team_member(
+    @firstName_from_form,
+    @lastName_from_form,
+    @homeLineID_from_dropdown,
+    @employmentStatus_from_dropdown,
+    @teamMemberID_out
+);
 
 /* Delete Team Member */
-
-DELETE FROM TeamMembers
-WHERE teamMemberID = @teamMemberID_of_row_of_button;
+CALL sp_delete_team_member(
+    @teamMemberID_of_row_of_button
+);
 
 /* Edit Team Member Prefill */
-
-SELECT TeamMembers.firstName, TeamMembers.lastName, AssemblyLines.lineName, TeamMembers.employmentStatus
+SELECT TeamMembers.teamMemberID,
+       TeamMembers.firstName,
+       TeamMembers.lastName,
+       TeamMembers.homeLineID,
+       AssemblyLines.lineName,
+       TeamMembers.employmentStatus
 FROM TeamMembers
-INNER JOIN AssemblyLines ON TeamMembers.homeLineID = AssemblyLines.lineID
+INNER JOIN AssemblyLines
+    ON TeamMembers.homeLineID = AssemblyLines.lineID
 WHERE TeamMembers.teamMemberID = @teamMemberID_from_row_or_button;
 
 /* Edit Team Member */
-
-UPDATE TeamMembers
-SET firstName = @firstName_from_form, lastName = @lastName_from_form,
-    homeLineID = @homeLineID_from_dropdown, employmentStatus = @employmentStatus_from_dropdown
-WHERE teamMemberID = @teamMemberID_of_row_of_button;
+CALL sp_update_team_member(
+    @teamMemberID_of_row_of_button,
+    @firstName_from_form,
+    @lastName_from_form,
+    @homeLineID_from_dropdown,
+    @employmentStatus_from_dropdown
+);
 
 /* Line Dropdown for position add/edit */
-
 SELECT lineID, lineName
 FROM AssemblyLines;
+
+
 /* ---------------------------------------------------
 Positions SQL
 -----------------------------------------------------*/
 
 /* Read existing Positions */
-
-SELECT Positions.positionID, Positions.positionName, AssemblyLines.lineName, Positions.requiredStaff, Skills.skillName
+SELECT Positions.positionID,
+       Positions.positionName,
+       AssemblyLines.lineName,
+       Positions.requiredStaff,
+       Skills.skillName
 FROM AssemblyLines
-INNER JOIN Positions ON AssemblyLines.lineID = Positions.lineID
-INNER JOIN Skills ON Positions.qualifyingSkillID = Skills.skillID;
+INNER JOIN Positions
+    ON AssemblyLines.lineID = Positions.lineID
+INNER JOIN Skills
+    ON Positions.qualifyingSkillID = Skills.skillID
+ORDER BY AssemblyLines.lineID, Positions.positionID;
 
 /* Skill Dropdown for position add */
-
-SELECT skillID, skillName FROM Skills;
+SELECT skillID, skillName
+FROM Skills;
 
 /* Add new Position */
-
-INSERT INTO Positions (positionName, lineID, requiredStaff, qualifyingSkillID)
-VALUES (@positionName_from_form, @lineID_from_dropdown, @requiredStaff_from_form, @qualifyingSkillID_from_dropdown);
+CALL sp_create_position(
+    @positionName_from_form,
+    @lineID_from_dropdown,
+    @requiredStaff_from_form,
+    @qualifyingSkillID_from_dropdown,
+    @positionID_out
+);
 
 /* Delete Position */
-
-DELETE FROM Positions
-WHERE positionID = @positionID_from_row_of_button;
+CALL sp_delete_position(
+    @positionID_from_row_of_button
+);
 
 /* Edit Position Pre-fill */
-
-SELECT Positions.positionName, AssemblyLines.lineName, Positions.requiredStaff, Skills.skillName
+SELECT Positions.positionID,
+       Positions.positionName,
+       Positions.lineID,
+       AssemblyLines.lineName,
+       Positions.requiredStaff,
+       Positions.qualifyingSkillID,
+       Skills.skillName
 FROM AssemblyLines
-INNER JOIN Positions ON AssemblyLines.lineID = Positions.lineID
-INNER JOIN Skills ON Positions.qualifyingSkillID = Skills.skillID
-WHERE positionID = @positionID_from_row_of_button;
+INNER JOIN Positions
+    ON AssemblyLines.lineID = Positions.lineID
+INNER JOIN Skills
+    ON Positions.qualifyingSkillID = Skills.skillID
+WHERE Positions.positionID = @positionID_from_row_of_button;
 
-/* edit position */
+/* Edit position */
+CALL sp_update_position(
+    @positionID_from_row_of_button,
+    @positionName_from_form,
+    @lineID_from_dropdown,
+    @requiredStaff_from_form,
+    @qualifyingSkillID_from_dropdown
+);
 
-UPDATE Positions
-SET positionName = @positionName_from_form, lineID = @lineID_from_dropdown,
-    requiredStaff = @requiredStaff_from_form, qualifyingSkillID = @qualifyingSkillID_from_dropdown
-WHERE positionID = @positionID_from_row_of_button;
 
 /* ---------------------------------------------------
 Skills SQL
 -----------------------------------------------------*/
 
-/* read existing skills */
-
+/* Read existing skills */
 SELECT skillID, skillName, skillDescription
 FROM Skills;
 
-/* Add new Line */
+/* Add new Skill */
+CALL sp_create_skill(
+    @skillName_from_form,
+    @skillDescription_from_form,
+    @skillID_out
+);
 
-INSERT INTO Skills (skillName, skillDescription)
-Values (@skillName_from_form, @skillDescription_from_form);
+/* Delete a Skill */
+CALL sp_delete_skill(
+    @skillID_of_row_of_button
+);
 
-/* Delete a Line */
-
-DELETE FROM Skills
-WHERE skillID = @skillID_of_row_of_button;
-
-/* Edit a line pre-fill*/
-
-SELECT skillID, skillName, skillDescription 
+/* Edit a Skill pre-fill */
+SELECT skillID, skillName, skillDescription
 FROM Skills
 WHERE skillID = @skillID_of_row_of_button;
 
-/* Update skill */
+/* Update Skill */
+CALL sp_update_skill(
+    @skillID_from_row_of_button,
+    @skillName_from_form,
+    @skillDescription_from_form
+);
 
-UPDATE Skills
-SET skillName = @skillName_from_form, skillDescription = @skillDescription_from_form
-WHERE skillID = @skillID_from_row_of_button;
 
 /* ---------------------------------------------------
 Team Member Skills SQL
 -----------------------------------------------------*/
 
 /* Read existing skill assignments */
-
-SELECT TeamMembers.firstName, TeamMembers.lastName, Skills.skillName
-FROM TeamMembers
-INNER JOIN TeamMemberSkills ON TeamMembers.teamMemberID = Skills.teamMemberID
-INNER JOIN Skills ON TeamMemberSkills.skillID = Skills.skillID;
+SELECT TeamMemberSkills.teamMemberID,
+       TeamMembers.firstName,
+       TeamMembers.lastName,
+       TeamMemberSkills.skillID,
+       Skills.skillName
+FROM TeamMemberSkills
+INNER JOIN TeamMembers
+    ON TeamMemberSkills.teamMemberID = TeamMembers.teamMemberID
+INNER JOIN Skills
+    ON TeamMemberSkills.skillID = Skills.skillID;
 
 /* Team Member dropdown */
-
 SELECT teamMemberID, firstName, lastName
 FROM TeamMembers;
 
 /* Skill dropdown */
-
 SELECT skillID, skillName
 FROM Skills;
 
-/* add new skill to TeamMember */
-
-INSERT INTO TeamMemberSkills (teamMemberID, skillID)
-VALUES (@teamMemberID_from_dropdown, @skillID_from_dropdown);
+/* Add new skill to TeamMember */
+CALL sp_create_tm_skill(
+    @teamMemberID_from_dropdown,
+    @skillID_from_dropdown
+);
 
 /* Remove skill from TeamMember */
+CALL sp_delete_tm_skill(
+    @teamMemberID_from_row_or_button,
+    @skillID_of_row_of_button
+);
 
-DELETE FROM TeamMemberSkills
-WHERE teamMemberID = @teamMemberID_from_row_or_button AND skillID = @skillID_of_row_of_button;
+/* Update TeamMember Skill */
+CALL sp_update_tm_skill(
+    @oldTeamMemberID,
+    @oldSkillID,
+    @newTeamMemberID,
+    @newSkillID
+);
+
 
 /* ---------------------------------------------------
 Schedules SQL
 -----------------------------------------------------*/
 
-/* read existing schedules */
-
-SELECT scheduleID, scheduleDate, shiftName
+/* Read existing schedules */
+SELECT scheduleID,
+       DATE_FORMAT(scheduleDate, '%m/%d/%Y') AS scheduleDate,
+       shiftName
 FROM Schedules;
 
-/* Add new Line */
+/* Add new Schedule */
+CALL sp_create_schedule(
+    @scheduleDate_from_form,
+    @shiftName_from_form,
+    @scheduleID_out
+);
 
-INSERT INTO Schedules (scheduleDate, shiftName)
-Values (@scheduleDate_from_form, @shiftName_from_form);
+/* Delete a Schedule */
+CALL sp_delete_schedule(
+    @scheduleID_of_row_of_button
+);
 
-/* Delete a Line */
-
-DELETE FROM Schedules
-WHERE scheduleID = @scheduleID_of_row_of_button;
-
-/* Edit a line pre-fill*/
-
-SELECT scheduleDate, shiftName 
+/* Edit a Schedule pre-fill */
+SELECT scheduleID, scheduleDate, shiftName
 FROM Schedules
 WHERE scheduleID = @scheduleID_of_row_of_button;
 
-/* Update skill */
+/* Update Schedule */
+CALL sp_update_schedule(
+    @scheduleID_from_row_of_button,
+    @scheduleDate_from_form,
+    @shiftName_from_form
+);
 
-UPDATE Schedules
-SET scheduleDate = @scheduleDate_from_form, shiftName = @shiftName_from_form
-WHERE scheduleID = @scheduleID_from_row_of_button;
 
 /* ---------------------------------------------------
 Daily Assignments SQL
 -----------------------------------------------------*/
 
-/* read all daily assignments */
-
-SELECT DailyAssignments.assignmentID, Schedules.scheduleDate, Schedules.shiftName, TeamMembers.firstName, TeamMembers.lastName, Positions.positionName
+/* Read all daily assignments */
+SELECT DailyAssignments.assignmentID,
+       DATE_FORMAT(Schedules.scheduleDate, '%m/%d/%Y') AS scheduleDate,
+       Schedules.shiftName,
+       TeamMembers.firstName,
+       TeamMembers.lastName,
+       Positions.positionName
 FROM Schedules
-INNER JOIN DailyAssignments ON Schedules.scheduleID = DailyAssignments.scheduleID
-INNER JOIN TeamMembers ON DailyAssignments.teamMemberID = TeamMembers.teamMemberID
-INNER JOIN Positions ON DailyAssignments.positionID = Positions.positionID;
+INNER JOIN DailyAssignments
+    ON Schedules.scheduleID = DailyAssignments.scheduleID
+INNER JOIN TeamMembers
+    ON DailyAssignments.teamMemberID = TeamMembers.teamMemberID
+INNER JOIN Positions
+    ON DailyAssignments.positionID = Positions.positionID
+ORDER BY Schedules.scheduleDate, TeamMembers.lastName;
 
-/* drop down for Schedules same as display query*/
-
-SELECT scheduleID, scheduleDate, shiftName
+/* Dropdown for Schedules */
+SELECT scheduleID,
+       DATE_FORMAT(scheduleDate, '%m/%d/%Y') AS scheduleDate,
+       shiftName
 FROM Schedules;
 
-/* drop down for Team Members */
-
+/* Dropdown for Team Members */
 SELECT teamMemberID, firstName, lastName
 FROM TeamMembers;
 
-/* drop down for positions */
-
-SELECT positionID, positionName FROM Positions;
+/* Dropdown for Positions */
+SELECT positionID, positionName
+FROM Positions;
 
 /* Insert new Daily Assignment */
+CALL sp_create_daily_assignment(
+    @scheduleID_from_dropdown,
+    @teamMemberID_from_dropdown,
+    @positionID_from_dropdown,
+    @assignmentID_out
+);
 
-INSERT INTO DailyAssignments (scheduleID, teamMemberID, positionID)
-VALUES (@scheduleID_from_dropdown, @teamMemberID_from_dropdown, @positionID_from_dropdown);
-
-/* Delete daily assignment */
-
-DELETE FROM DailyAssignments
-WHERE assignmentID = @assignmentID_from_row_of_button;
+/* Delete Daily Assignment */
+CALL sp_delete_daily_assignment(
+    @assignmentID_from_row_of_button
+);
 
 /* Edit Daily Assignment Prefill */
-
-SELECT Schedules.scheduleDate, Schedules.shiftName, TeamMembers.firstName, TeamMembers.lastName, Positions.positionName
+SELECT DailyAssignments.assignmentID,
+       DailyAssignments.scheduleID,
+       DailyAssignments.teamMemberID,
+       DailyAssignments.positionID,
+       Schedules.scheduleDate,
+       Schedules.shiftName,
+       TeamMembers.firstName,
+       TeamMembers.lastName,
+       Positions.positionName
 FROM Schedules
-INNER JOIN DailyAssignments ON Schedules.scheduleID = DailyAssignments.scheduleID
-INNER JOIN TeamMembers ON DailyAssignments.teamMemberID = TeamMembers.teamMemberID
-INNER JOIN Positions ON DailyAssignments.positionID = Positions.positionID
+INNER JOIN DailyAssignments
+    ON Schedules.scheduleID = DailyAssignments.scheduleID
+INNER JOIN TeamMembers
+    ON DailyAssignments.teamMemberID = TeamMembers.teamMemberID
+INNER JOIN Positions
+    ON DailyAssignments.positionID = Positions.positionID
 WHERE DailyAssignments.assignmentID = @assignmentID_from_row_of_button;
 
 /* Update Daily Assignment */
-
-UPDATE DailyAssignments
-SET scheduleID = @scheduleID_from_dropdown, teamMemberID = @teamMemberID_from_dropdown, positionID = @positionID_from_dropdown
-WHERE assignmentID = assignmentID_from_row_of_button;
+CALL sp_update_daily_assignment(
+    @assignmentID_from_row_of_button,
+    @scheduleID_from_dropdown,
+    @teamMemberID_from_dropdown,
+    @positionID_from_dropdown
+);
